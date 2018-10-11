468bd2c39e19b151120bf4dbc4c76fc2734ef593
Jerry <wzjing103@163.com>
2018-10-11 17:48:23 +0800

    initial commit

---
+++ test.h
@@ -0,0 +1,12 @@
+//
+// Created by android1 on 2018/10/11.
+//
+
+#ifndef VIDEOBOX_TEST_H
+#define VIDEOBOX_TEST_H
+
+#include <libavutil/frame.h>
+
+int create_snapshot(char * filename);
+
+#endif //VIDEOBOX_TEST_H
