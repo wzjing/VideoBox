14c7aee7cae2a456ea68c93e7ced2d066e522893
Jerry <wzjing103@163.com>
2019-05-05 17:57:48 +0800

    update video mux

--- src/muxing/muxing.cpp
+++ src/muxing/muxing.cpp
@@ -3,8 +3,159 @@
 //
 
 #include "muxing.h"
+#include "../utils/log.h"
 
-Muxer *get_muxer(const char *out_filename) {
+Muxer *open_muxer(const char *out_filename) {
+  auto *muxer = (Muxer *) malloc(sizeof(Muxer));
+  AVFormatContext *fmt_ctx;
+  int ret = 0;
+
+  avformat_alloc_output_context2(&fmt_ctx, nullptr, nullptr, out_filename);
+
+
+  if (!(fmt_ctx->oformat->flags & AVFMT_NOFILE)) {
+    LOGD("Opening file: %s\n", out_filename);
+    ret = avio_open(&fmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
+    if (ret < 0) {
+      LOGE("could not open %s (%s)\n", out_filename, av_err2str(ret));
+      free(muxer);
+      return nullptr;
+    }
+  }
+
+  muxer->output_filename = out_filename;
+  muxer->fmt_ctx = fmt_ctx;
 
   return nullptr;
+}
+
+int add_media(Muxer *muxer, MediaConfig *config) {
+
+  if (config == nullptr || muxer == nullptr) {
+    LOGE("Error: muxer or media config is nullptr\n");
+    return -1;
+  }
+
+  int ret;
+
+  auto *media = (Media *) malloc(sizeof(Media));
+  AVCodec *codec = avcodec_find_encoder(config->codec_id);
+  media->media_type = config->media_type;
+
+  AVStream *stream = avformat_new_stream(muxer->fmt_ctx, nullptr);
+  if (!stream) {
+    LOGE("could not find create stream\n");
+    free(media);
+    return -1;
+  }
+  stream->id = muxer->fmt_ctx->nb_streams - 1;
+  AVCodecContext *codec_ctx = avcodec_alloc_context3(codec);
+  if (!codec_ctx) {
+    LOGE("could not alloc encode context\n");
+    free(media);
+    return -1;
+  }
+
+  AVFrame *frame = av_frame_alloc();
+  switch (config->media_type) {
+    case AVMEDIA_TYPE_VIDEO:
+      stream->time_base = (AVRational) {1, config->frame_rate};
+      codec_ctx->pix_fmt = (AVPixelFormat) config->format;
+      codec_ctx->time_base = stream->time_base;
+      codec_ctx->codec_id = config->codec_id;
+      codec_ctx->bit_rate = config->bit_rate;
+      codec_ctx->width = config->width;
+      codec_ctx->height = config->height;
+      codec_ctx->gop_size = config->gop_size;
+
+      ret = avcodec_open2(codec_ctx, codec, nullptr);
+      if (ret < 0) {
+        LOGE("unable to open codec: %s\n", av_err2str(ret));
+        free(media);
+        return -1;
+      }
+
+      ret = avcodec_parameters_from_context(stream->codecpar, codec_ctx);
+      if (ret < 0) {
+        LOGE("unable to copy stream parameter to AVCodecContext: %s\n", av_err2str(ret));
+        return -1;
+      }
+
+      // alloc frame
+      if (!frame) {
+        LOGE("unable to alloc frame\n");
+        free(media);
+        return -1;
+      }
+
+      frame->format = config->format;
+      frame->width = config->width;
+      frame->height = config->height;
+
+      ret = av_frame_get_buffer(frame, 0);
+      if (ret < 0) {
+        LOGE("unable to alloc buffer for frame: %s\n", av_err2str(ret));
+        return -1;
+      }
+
+      break;
+    case AVMEDIA_TYPE_AUDIO:
+      codec_ctx->sample_fmt = (AVSampleFormat) config->format;
+      codec_ctx->codec_id = config->codec_id;
+      codec_ctx->bit_rate = config->bit_rate;
+      codec_ctx->sample_rate = config->sample_rate;
+      codec_ctx->channel_layout = config->channel_layout;
+      codec_ctx->channels = av_get_channel_layout_nb_channels(config->channel_layout);
+      stream->time_base = (AVRational) {1, codec_ctx->sample_rate};
+      codec_ctx->time_base = stream->time_base;
+
+      ret = avcodec_open2(codec_ctx, codec, nullptr);
+      if (ret < 0) {
+        LOGE("unable to open codec: %s\n", av_err2str(ret));
+        free(media);
+        return -1;
+      }
+
+      ret = avcodec_parameters_from_context(stream->codecpar, codec_ctx);
+      if (ret < 0) {
+        LOGE("unable to copy stream parameter to AVCodecContext: %s\n", av_err2str(ret));
+        return -1;
+      }
+
+      frame->format = config->format;
+      frame->channel_layout = config->channel_layout;
+      frame->sample_rate = config->sample_rate;
+      frame->nb_samples = config->nb_samples;
+
+      ret = av_frame_get_buffer(frame, 0);
+      if (ret < 0) {
+        LOGE("unable to alloc buffer for frame: %s\n", av_err2str(ret));
+        return -1;
+      }
+      break;
+    case AVMEDIA_TYPE_DATA:
+      break;
+    case AVMEDIA_TYPE_SUBTITLE:
+      break;
+    case AVMEDIA_TYPE_ATTACHMENT:
+      break;
+    case AVMEDIA_TYPE_NB:
+      break;
+    case AVMEDIA_TYPE_UNKNOWN:
+      break;
+  }
+  if (muxer->fmt_ctx->oformat->flags & AVFMT_GLOBALHEADER) {
+    codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+  }
+  media->stream = stream;
+  media->codec_ctx = codec_ctx;
+  media->media_type = config->media_type;
+  media->stream_idx = stream->index;
+  media->frame = frame;
+
+  return 1;
+}
+
+int mux(Muxer *muxer, MUX_CALLBACK callback) {
+  return 0;
 }
--- src/muxing/muxing.h
+++ src/muxing/muxing.h
@@ -5,39 +5,64 @@
 #ifndef VIDEOBOX_MUXING_H
 #define VIDEOBOX_MUXING_H
 
+#include <functional>
+
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
 }
 
+typedef struct MediaConfig {
+  AVMediaType media_type;
+  AVCodecID codec_id;
+
+  // Pixel format for video, sample format for audio
+  int format;
+
+  // bit rate decide the quality, higher is better
+  int64_t bit_rate;
+
+  // only for audio
+  int sample_rate; // frame rate in byte: 44100, 48000...
+  int nb_samples; // samples per frame: 1024 for aac
+  int64_t channel_layout;  // channel layout
+
+  // only for video
+  int height;
+  int width;
+  int frame_rate; // frames per second
+  int gop_size;
+} MediaConfig;
+
 typedef struct Media {
-    uint stream_idx;
-    AVMediaType media_type;
-    AVCodecContext *codec_ctx;
-    AVStream *stream;
-    int64_t next_pts;
-    AVFrame *frame;
+  uint stream_idx;
+  AVMediaType media_type;
+  AVCodecContext *codec_ctx;
+  AVStream *stream;
+  int64_t next_pts;
+  AVFrame *frame;
 } Media;
 
 typedef struct Muxer {
-    const char *output_filename;
-    AVFormatContext *fmt_ctx;
-    Media **media;
-    uint nb_media;
+  const char *output_filename;
+  AVFormatContext *fmt_ctx;
+  Media **media;
+  uint nb_media;
 
-    int *video_ids;
-    int *audio_ids;
+  int *video_ids;
+  int *audio_ids;
 
 } Muxer;
 
-Muxer *get_muxer(const char *out_filename);
+//
+typedef const std::function<AVPacket(int available_media_type)>& MUX_CALLBACK;
 
-void add_media(Muxer *muxer, Media *media);
+Muxer *open_muxer(const char *out_filename);
 
-void open_muxer(Muxer *muxer);
+int add_media(Muxer *muxer, MediaConfig *config);
 
-int add_packet(Muxer *muxer, AVPacket *packet);
+int mux(Muxer *muxer, MUX_CALLBACK callback);
 
 void close_muxer(Muxer *muxer);
 
